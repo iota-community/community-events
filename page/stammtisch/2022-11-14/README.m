805fca4809aa129419daf151fb03e8d08d89648a
vromvonbeyond <45822157+vromvonbeyond@users.noreply.github.com>
2022-11-08 11:16:13 +0100

    Update README.m

--- page/stammtisch/2022-11-14/README.m
+++ page/stammtisch/2022-11-14/README.m
@@ -1,4 +1,4 @@
 ## IOTA Stammtischthemen für Montag, den 14.11.2022
 
-1. **IOTA Quicktakes**: [IF Tweets](https://twitter.com/iota/status/1589558401608065026?s=20&t=p0b9tGAharZB0FUpZosxzw)
+1. **IOTA Quicktakes**: [IF Tweets](https://twitter.com/iota/status/1589558401608065026?s=20&t=p0b9tGAharZB0FUpZosxzw); Youtube: [IOTA Quicktakes 07.11.2022: Stronghold Release, AuditOne Joins Touchpoint, Assembly Staking #4](https://www.youtube.com/watch?v=c7OEFJJhMq8)
 2. **IOTA Youtube**: [BC100+ (Blockchain for UN Values and Global Goals) Initiative Presentation at COP27 DIP](https://www.youtube.com/watch?v=wwC8FQ5-4ko&feature=youtu.be)
