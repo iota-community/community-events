bce7cb6119ec388d747f669601dc886ebbc1178a
vromvonbeyond <45822157+vromvonbeyond@users.noreply.github.com>
2022-11-08 11:12:29 +0100

    Create README.m

---
+++ page/stammtisch/2022-11-14/README.m
@@ -0,0 +1,4 @@
+## IOTA Stammtischthemen für Montag, den 14.11.2022
+
+1. **IOTA Quicktakes**: [IF Tweets](https://twitter.com/iota/status/1589558401608065026?s=20&t=p0b9tGAharZB0FUpZosxzw)
+2. **IOTA Youtube**: [BC100+ (Blockchain for UN Values and Global Goals) Initiative Presentation at COP27 DIP](https://www.youtube.com/watch?v=wwC8FQ5-4ko&feature=youtu.be)
