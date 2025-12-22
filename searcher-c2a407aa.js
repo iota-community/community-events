e5b09d6c0518f573c584f6a8bc1bb36185994937
vromvonbeyond <vromvonbeyond@users.noreply.github.com>
2025-12-22 11:26:07 +0000

    deploy: b71b4dc75a1eccd64656dd40ec0fa13029684d65

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-9c385a96.js',
+                path_to_root + 'searchindex-6029ad7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
